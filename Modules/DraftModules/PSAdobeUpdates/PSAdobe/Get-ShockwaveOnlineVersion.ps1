e9e14e3b2d94e74ef537678cecccb2bcab1ed84f
Kreloc <kingpincat@hotmail.com>
2016-03-29 09:12:00 -0800

    Change
    Changed some adobe version functions

--- Modules/DraftModules/PSAdobeUpdates/PSAdobe/Get-ShockwaveOnlineVersion.ps1
+++ Modules/DraftModules/PSAdobeUpdates/PSAdobe/Get-ShockwaveOnlineVersion.ps1
@@ -24,18 +24,22 @@ Function Get-ShockwaveOnlineVersion
 	(
 		[Parameter(Mandatory=$False,ValueFromPipelinebyPropertyName=$true)]
 		[alias("Url")]
-		[string]$Uri = "https://helpx.adobe.com/shockwave.html"
+		[string]$Uri = "https://get.adobe.com/shockwave/"
 	)
 	Begin{}
 	Process 
 	{
 		$Shockwave = Invoke-WebRequest -Uri $Uri
- 		$ShockWaveVersionPage = "https://helpx.adobe.com" + ($Shockwave.Links | Where {$_.outerText -like "Release Notes*"}).href
+ 		<#Below is kind of broken, does report the newest version on that page but that page is not up to date anymore.
+        #$uri = "https://helpx.adobe.com/shockwave.html"
+        $ShockWaveVersionPage = "https://helpx.adobe.com" + ($Shockwave.Links | Where {$_.outerText -like "Release Notes*"}).href
         $ShockWaveVersion = Invoke-WebRequest -Uri $ShockWaveVersionPage
         $regex = "Shockwave Player.*"
         $ShockWaveVersions = [regex]::matches($ShockWaveVersion, $regex, "IgnoreCase")
-		$CurrentVersionUgly = ($ShockwaveVersions | Where {$_.Value -match "\d+" -and $_.Value -like "Shockwave Player*" -and $_.Value -notlike "Shockwave Player 11*"} | Sort Index)[0]
+		$CurrentVersionUgly = ($ShockwaveVersions | Where {$_.Value -match "\d+" -and $_.Value -like "Shockwave Player*" -and $_.Value -notlike "Shockwave Player 11*" -and $_.Value -match '&nbsp;'} | Sort Index)[0]
 		$ShockWaveCurrentVersion = (($CurrentVersionUgly -split '&nbsp;')[1]).replace('<br />',"")
+        #>
+        $ShockWaveCurrentVersion = ($Shockwave.ParsedHtml.getElementById("autoSelectedVersion").textContent -replace "System.*","") -replace "Version ",""
 		New-Object -TypeName PSOBject -Property @{ShockWavePlayerVersion=$ShockWaveCurrentVersion}
 	}
 	End{}
